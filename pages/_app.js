5ea72aefb4decb5d8b850dff15304768de6a9c01
Edgar <elindo586@gmail.com>
2022-05-25 15:43:53 -0400

    inventory data not working

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
     <div>
       <div>
         <Head>
-          <meta charset="utf-8" />
+          <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
             rel="shortcut icon"
--- pages/inventory.js
+++ pages/inventory.js
@@ -3,8 +3,180 @@ import { useEffect, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Pagination from "react-bootstrap/Pagination";
+import { ConnectionToDatabase } from "../utils/connection";
 
-const Inventory = () => {
+export const getStaticProps = async () => {
+  const res = await ConnectionToDatabase;
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+    revalidate: 60,
+  };
+};
+
+const Inventory = ({ data }) => {
+  const [isActive, setIsActive] = useState([]);
+  const [isInventory, setIsInventory] = useState([]);
+  const [savedParts, setSavedParts] = useState([]);
+
+  let partsList = [];
+  // store inventory items
+  let itemsPerPage = [];
+  // store count as an array
+  let activepages = [];
+
+  // pagination elements stored
+  let paginatedElements = [];
+  // activeInventoryElements stored
+  let activeInventory = [];
+
+  let pages;
+
+  let itemStart = 0;
+  let itemEnd = 24;
+  useEffect(() => {
+    partsList = data;
+    setSavedParts(partsList);
+    // get pages
+    for (let i = 1; i <= pages; i++) {
+      activepages.push(i);
+    }
+    // get 25 items
+    renderItems();
+    // create paginated elements
+    getPagination();
+  }, []);
+
+  // fetch data from server
+  // const getInventory = async () => {
+  //   await fetch("/api/parts/getallparts")
+  //     .then((data) => {
+  //       partsList = data;
+  //       setSavedParts(partsList)
+  //     })
+  // }
+
+  const getPagination = () => {
+    // get pages
+    if (activeInventory.length > 0) {
+      pages = Math.ceil(activeInventory.length / 25);
+    } else {
+      pages = Math.ceil(partsList.length / 25);
+    }
+
+    for (let i = 1; i <= pages; i++) {
+      paginatedElements.push(
+        <Pagination.Item key={i} id={i} onClick={getActiveItems}>
+          {i}
+        </Pagination.Item>
+      );
+    }
+    setIsActive(paginatedElements);
+  };
+
+  // return 25 items according to
+  const getActiveItems = () => {
+    // capture url
+    let pageId = document.activeElement.id;
+
+    if (itemStart === 0) {
+      itemEnd = pageId * 25 - 1;
+      itemStart = pageId * 25 - 25;
+    }
+
+    renderItems();
+
+    itemStart = 0;
+    itemEnd = 24;
+  };
+
+  const renderItems = () => {
+    itemsPerPage = [];
+    if (itemsPerPage === isInventory) {
+      setIsInventory([]);
+    }
+
+    // get 25 items
+    if (activeInventory.length > 0) {
+      for (let i = itemStart; i <= itemEnd; i++) {
+        if (activeInventory[i]) {
+          itemsPerPage.push(activeInventory[i]);
+        } else {
+          break;
+        }
+      }
+      setIsInventory(itemsPerPage);
+    } else {
+      for (let i = itemStart; i <= itemEnd; i++) {
+        if (partsList[i]) {
+          itemsPerPage.push(
+            <tr key={`tr${i}`}>
+              <td key={`ref${i}`}>{partsList[i].Reference}</td>
+              <td key={`desc${i}`}>{partsList[i].Description}</td>
+              <td key={`repl${i}`}>{partsList[i].Replacements}</td>
+            </tr>
+          );
+        } else {
+          break;
+        }
+      }
+      setIsInventory(itemsPerPage);
+    }
+  };
+
+  const searchItems = () => {
+    let searching = document.activeElement.value;
+
+    searching = searching.toString(searching);
+    searching = searching.toLowerCase();
+    searching = searching.trim(" ");
+
+    itemsPerPage = [];
+    partsList = savedParts;
+    if (searching.length === 0) {
+      console.log("nope");
+      partsList = savedParts;
+      renderItems();
+      getPagination();
+    } else {
+      for (let i = 0; i <= savedParts.length; i++) {
+        if (savedParts[i]) {
+          // searching Reference number
+          let refSearch = savedParts[i].Reference;
+          refSearch = refSearch.toString(refSearch);
+          refSearch = refSearch.toLowerCase(refSearch);
+
+          // searching Reference number
+          let descSearch = savedParts[i].Description;
+          descSearch = descSearch.toString(descSearch);
+          descSearch = descSearch.toLowerCase(descSearch);
+
+          // searching Reference number
+          let replSearch = savedParts[i].Replacements;
+          replSearch = replSearch.toString(replSearch);
+          replSearch = replSearch.toLowerCase(replSearch);
+
+          if (
+            refSearch.match(searching) ||
+            descSearch.match(searching) ||
+            replSearch.match(searching)
+          ) {
+            activeInventory.push(
+              <tr key={`tr${i}`}>
+                <td key={`ref${i}`}>{savedParts[i].Reference}</td>
+                <td key={`desc${i}`}>{savedParts[i].Description}</td>
+                <td key={`repl${i}`}>{savedParts[i].Replacements}</td>
+              </tr>
+            );
+          }
+        }
+      }
+      renderItems();
+      getPagination();
+    }
+  };
   return (
     <div>
       <div>
@@ -32,15 +204,45 @@ const Inventory = () => {
             Here is a quick overview of regular components coming from our
             inventory. If you have any requirements on these components you can
             always
-            <Link href="/contact-us">
-              <a> contact us </a>
-            </Link>
-            for a quote request.
+            <Link href="/contact">contact us for a quote request.</Link>
           </p>
         </div>
-        <div className="col-md-12"></div>
-        <div className="col-md-12"></div>
-        <div className="col-md12"></div>
+        <div className="col-md-12">
+          <input
+            type="text"
+            id="myInput"
+            // onKeyUp="myFunction()"
+            onKeyUp={searchItems}
+            placeholder="Search for Part Number.."
+            title="Type in a name"
+          />
+          <br />
+          <br />
+
+          <table
+            className="my-talbe-sort table table-hover table-striped table-bordered"
+            id="myTable"
+          >
+            <thead>
+              <tr>
+                <th>Reference Number</th>
+                <th>Description / Long Part Number</th>
+                <th>Alternative replacement for:</th>
+              </tr>
+              {/* {savedParts.map((item, index) => (
+                <tr>
+              <th>Reference Number</th>
+              <th>Description / Long Part Number</th>
+              <th>Alternative replacement for:</th>
+            </tr>
+              ))} */}
+            </thead>
+
+            <tbody>{isInventory}</tbody>
+          </table>
+
+          <Pagination>{isActive}</Pagination>
+        </div>
       </div>
     </div>
   );
