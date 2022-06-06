34c10ecd69541527aa8d302d039882123a6c89a7
Edgar <elindo586@gmail.com>
2022-06-06 13:00:26 -0400

    not working

--- pages/inventory.js
+++ pages/inventory.js
@@ -3,9 +3,26 @@ import { useEffect, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Pagination from "react-bootstrap/Pagination";
-import { PartsData } from "./PartsData";
+import { clientPromise } from "../utils/mongodb";
 
-const Inventory = () => {
+export async function getStaticProps() {
+  // await clientPromise;
+  // `await clientPromise` will use the default database passed in the MONGODB_URI
+  // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+  //
+  const client = await clientPromise;
+  const db = client.db("parts");
+  //
+  // Then you can execute queries against your database like so:
+  // db.find({}) or any of the MongoDB Node Driver commands
+
+  return {
+    props: { db },
+    revalidate: 60,
+  };
+}
+
+const Inventory = ({ db }) => {
   const [isActive, setIsActive] = useState([]);
   const [isInventory, setIsInventory] = useState([]);
   const [savedParts, setSavedParts] = useState([]);
@@ -46,7 +63,7 @@ const Inventory = () => {
   }, []);
 
   // fetch data from server
-  const getInventory = { PartsData }
+  const getInventory = { db }
     .then((response) => {
       if (!response.ok) {
         return response.statusText();
