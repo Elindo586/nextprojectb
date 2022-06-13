d22f2c6c84f902fcd783c290a3c752a04585a24e
Edgar <elindo586@gmail.com>
2022-06-12 20:56:11 -0400

    redirect OK

---
+++ pages/_404/[...path].js
@@ -0,0 +1,8 @@
+// renders nada
+export const Custom404 = () => <div></div>;
+
+export const getServerSideProps = () => {
+  return { redirect: { destination: "/", permanent: false } };
+};
+
+export default Custom404;
