492dadf479c1ccc2dbfb991bad159c8237ca0901
Edgar <elindo586@gmail.com>
2022-08-25 12:50:02 -0400

    icons pwa

--- pages/_app.js
+++ pages/_app.js
@@ -35,6 +35,90 @@ function MyApp({ Component, pageProps }) {
       <div>
         <Head>
           <link rel="manifest" href="/manifest.json" />
+
+          {/* IOS */}
+          <meta name="format-detection" content="telephone=no"></meta>
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/io/icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/io/icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/io/icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/io/icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/io/icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/io/icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/io/icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/io/icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/io/icon-180x180.png"
+          />
+          <meta
+            name="apple-mobile-web-app-title"
+            content="Technical Union"
+          ></meta>
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black"
+          ></meta>
+
+          {/* ANDROID */}
+          <meta name="mobile-web-app-capable" content="yes" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/android/icon-16x16.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/android/icon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/android/icon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/android/icon-192x192.png"
+          />
         </Head>
       </div>
       <div>
