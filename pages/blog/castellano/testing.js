b69aa7bf5ef4ffdeae1be7e655d80dd61dbcb51e
Edgar <elindo586@gmail.com>
2022-08-29 15:58:21 -0400

    test

--- pages/blog/castellano/testing.js
+++ pages/blog/castellano/testing.js
@@ -9,8 +9,53 @@ export async function getStaticProps() {
   };
 }
 
-const PostTest = (db) => {
-  return <Blogger11 />;
+const PostTest = ({ db }) => {
+  function post(info) {
+    return (
+      info.id === "4995",
+      info.item,
+      info.metaTitle,
+      info.metaDescription,
+      info.metaKeywords,
+      info.ogTitle,
+      info.ogDescription,
+      info.ogURL,
+      info.ogImage,
+      info.twitterTitle,
+      info.twitterDescription,
+      info.twitterImage,
+      info.title,
+      info.body,
+      info.description,
+      info.previous,
+      info.next
+    );
+  }
+
+  const item = db.find(post);
+
+  return (
+    <div>
+      <Blogger11
+        key={item.id}
+        metaTitle={item.metaTitle}
+        metaDescription={item.metaDescription}
+        metaKeywords={item.metaKeywords}
+        ogTitle={item.ogTitle}
+        ogDescription={item.ogDescription}
+        ogURL={item.ogURL}
+        ogImage={item.ogImage}
+        twitterTitle={item.twitterTitle}
+        twitterDescription={item.twitterDescription}
+        twitterImage={item.twitterImage}
+        title={item.title}
+        body={item.body}
+        description={item.description}
+        previous={item.previous}
+        next={item.next}
+      />
+    </div>
+  );
 };
 
 export default PostTest;
