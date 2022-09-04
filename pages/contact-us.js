e84c8ceb11e1450b85fc8aaf7684e5b3214c835d
Edgar <elindo586@gmail.com>
2022-09-04 09:50:11 -0400

    fixes

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -8,6 +8,7 @@ const Contact = () => {
     <div>
       <div>
         <Head>
+          <title>TU-Technical Union | Contact US.</title>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
@@ -71,6 +72,7 @@ const Contact = () => {
         <div className="col-md-12">
           <div className="ratio ratio-21x9">
             <iframe
+              title="Find Technical Union"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d376620.65888018865!2d-83.65734297763709!3d42.48413467775062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824ae2f8c7e3c7f%3A0x1978ab227e003bbc!2sFarmington%20Hills%2C%20MI!5e0!3m2!1sen!2sus!4v1640206161066!5m2!1sen!2sus"
               allowFullScreen=""
               loading="lazy"
