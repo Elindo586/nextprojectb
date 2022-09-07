f670734758d808a62f26e7870a3d7d880a3b3286
Edgar <elindo586@gmail.com>
2022-09-07 14:58:30 -0400

    ititle

--- components/blog-post-spanish.js
+++ components/blog-post-spanish.js
@@ -72,7 +72,7 @@ function Blogger1(props) {
               width="560"
               height="315"
               src={`${props.body}`}
-              title="YouTube video player"
+              title={`${props.iTitle}`}
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
