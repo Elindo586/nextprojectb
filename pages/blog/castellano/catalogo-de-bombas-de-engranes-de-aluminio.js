47475f8f88577511f20eb0ce295a50873726a287
Edgar <elindo586@gmail.com>
2022-09-15 10:01:42 -0400

    blogs

---
+++ pages/blog/castellano/catalogo-de-bombas-de-engranes-de-aluminio.js
@@ -0,0 +1,74 @@
+import React from "react";
+// import { useEffect } from "react";
+import Blogger1 from "../../../components/blog-post-spanish";
+
+import db from "../../../utils/blogs-front/spanish/blog-spanish.json";
+
+export async function getStaticProps() {
+  return {
+    props: { db },
+  };
+}
+
+const PostSpanish49 = ({ db }) => {
+  // This is another way to do it if I dont do filter below
+  // function post(info) {
+  //   return (
+  //     info.id === "5",
+  //     info.item,
+  //     info.metaTitle,
+  //     info.metaDescription,
+  //     info.metaKeywords,
+  //     info.ogTitle,
+  //     info.ogDescription,
+  //     info.ogURL,
+  //     info.ogImage,
+  //     info.twitterTitle,
+  //     info.twitterDescription,
+  //     info.twitterImage,
+  //     info.title,
+  //     info.body,
+  //     info.description,
+  //     info.previous,
+  //     info.next
+  //   );
+  // }
+
+  // const item = db.find(post);
+
+  return (
+    <div>
+      {db
+        .filter((item) => item.id === "49")
+        .map((item) => {
+          return (
+            <Blogger1
+              key={item.id}
+              metaTitle={item.metaTitle}
+              metaDescription={item.metaDescription}
+              metaKeywords={item.metaKeywords}
+              ogTitle={item.ogTitle}
+              ogDescription={item.ogDescription}
+              ogURL={item.ogURL}
+              ogImage={item.ogImage}
+              twitterTitle={item.twitterTitle}
+              twitterDescription={item.twitterDescription}
+              twitterImage={item.twitterImage}
+              title={item.title}
+              body={item.body}
+              iTitle={item.iTitle}
+              description={
+                <div
+                  dangerouslySetInnerHTML={{ __html: item.description }}
+                ></div>
+              }
+              previous={item.previous}
+              next={item.next}
+            />
+          );
+        })}
+    </div>
+  );
+};
+
+export default PostSpanish49;
