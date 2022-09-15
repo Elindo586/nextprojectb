fb0ca98befe171922f2bf58928ca296f39b05298
Edgar <elindo586@gmail.com>
2022-09-15 13:04:29 -0400

    blogs

---
+++ pages/blog/english/what-is-1714-in-hydraulics.js
@@ -0,0 +1,70 @@
+import React from "react";
+// import { useEffect } from "react";
+import Blogger2 from "../../../components/blog-post-english";
+
+import db from "../../../utils/blogs-front/english/blog-english.json";
+
+export async function getStaticProps() {
+  return {
+    props: { db },
+  };
+}
+
+const PostEnglish51 = ({ db }) => {
+  // This is another way to do it if I dont do filter below
+  // function post(info) {
+  //   return (
+  //     info.id === "5",
+  //     info.item,
+  //     info.metaTitle,
+  //     info.metaDescription,
+  //     info.metaKeywords,
+  //     info.ogTitle,
+  //     info.ogDescription,
+  //     info.ogURL,
+  //     info.ogImage,
+  //     info.twitterTitle,
+  //     info.twitterDescription,
+  //     info.twitterImage,
+  //     info.title,
+  //     info.body,
+  //     info.description,
+  //     info.previous,
+  //     info.next
+  //   );
+  // }
+
+  // const item = db.find(post);
+
+  return (
+    <div>
+      {db
+        .filter((item) => item.id === "51")
+        .map((item) => {
+          return (
+            <Blogger2
+              key={item.id}
+              metaTitle={item.metaTitle}
+              metaDescription={item.metaDescription}
+              metaKeywords={item.metaKeywords}
+              ogTitle={item.ogTitle}
+              ogDescription={item.ogDescription}
+              ogURL={item.ogURL}
+              ogImage={item.ogImage}
+              twitterTitle={item.twitterTitle}
+              twitterDescription={item.twitterDescription}
+              twitterImage={item.twitterImage}
+              title={item.title}
+              body={item.body}
+              iTitle={item.iTitle}
+              description={item.description}
+              previous={item.previous}
+              next={item.next}
+            />
+          );
+        })}
+    </div>
+  );
+};
+
+export default PostEnglish51;
