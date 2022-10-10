0b4e97ebb292eb814bb77d87137a2f9b58b5eab7
Edgar <elindo586@gmail.com>
2022-10-10 12:01:38 -0400

    social

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -17,21 +18,100 @@ const Footer = () => {
             <Col className="footer-privacy">PRIVACY POLICY</Col>
           </Row>
           <Row>
-            {" "}
             <Col>
               <p className="copyright">
                 Copyright © {fullYear()} TU - All Rights Reserved{" "}
               </p>
             </Col>
             <Col className="dev-by">
-              <span className="dev-by2">Developed by:</span>{" "}
-              <a
-                className="edgar"
-                href="https://www.edgarlindo.com/"
-                target="blank"
-              >
-                Edgar Lindo
-              </a>
+              <Row>
+                <Col>
+                  <span className="dev-by2">Developed by:</span>{" "}
+                  <a
+                    className="edgar"
+                    href="https://www.edgarlindo.com/"
+                    target="blank"
+                  >
+                    Edgar Lindo
+                  </a>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="social-icon-col">
+                  <a
+                    className="footer-icon"
+                    href="https://www.linkedin.com/in/edgarlindo"
+                    target="blank"
+                  >
+                    <Image
+                      src="/images/footer-images/footer-linkedin.png"
+                      id="youtube-spanish"
+                      alt="youtube spanish logo"
+                      width={25}
+                      height={25}
+                      className="footer-icon"
+                      title="LinkedIn"
+                    />
+                  </a>
+
+                  <a
+                    className="footer-icon"
+                    href="https://twitter.com/MrEdgarLindo"
+                    target="blank"
+                  >
+                    <Image
+                      src="/images/footer-images/footer-twitter.png"
+                      id="youtube-spanish"
+                      alt="youtube spanish logo"
+                      width={25}
+                      height={25}
+                      title="Twitter"
+                    />
+                  </a>
+                  <a
+                    className="footer-icon"
+                    href="https://www.facebook.com/RealEdgarLindo"
+                    target="blank"
+                  >
+                    <Image
+                      src="/images/footer-images/footer-facebook.png"
+                      id="youtube-spanish"
+                      alt="youtube spanish logo"
+                      width={25}
+                      height={25}
+                      title="Facebook"
+                    />
+                  </a>
+                  <a
+                    className="footer-icon"
+                    href="https://www.youtube.com/c/EdgarLindoEL/videos"
+                    target="blank"
+                  >
+                    <Image
+                      src="/images/footer-images/footer-youtube.png"
+                      id="youtube-spanish"
+                      alt="youtube spanish logo"
+                      width={25}
+                      height={25}
+                      title="YouTube English"
+                    />
+                  </a>
+                  <a
+                    className="footer-icon"
+                    href="https://www.youtube.com/c/EdgarLindo/videos"
+                    target="blank"
+                  >
+                    <Image
+                      src="/images/footer-images/footer-youtube.png"
+                      id="youtube-spanish"
+                      alt="youtube spanish logo"
+                      width={25}
+                      height={25}
+                      title="YouTube Spanish"
+                    />
+                  </a>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Container>
