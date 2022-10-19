9e7673e437a3fb54064b843fa76a17d9e09c08c9
Edgar <elindo586@gmail.com>
2022-10-19 14:51:23 -0400

    this

--- pages/this/hydrocraft.js
+++ pages/this/hydrocraft.js
@@ -8,7 +8,65 @@ const HydroCraft = () => {
   return (
     <div>
       <div>
-        <Head></Head>
+        <Head>
+          {" "}
+          <div>
+            {/* Main */}
+            <meta charset="UTF-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <link
+              rel="shortcut icon"
+              type="image/png"
+              href="/images/tu-favicon.png"
+            />
+
+            <meta
+              name="facebook-domain-verification"
+              content="fzctnjbrtlybvytmamk8glkng9af7p"
+            />
+            <title>HYDROCRAFT</title>
+            <meta
+              name="description"
+              content="This is the house of HydroCraft.com"
+            />
+            <meta name="keywords" content="hydrocraft" />
+            <meta name="author" content="Edgar Lindo" />
+
+            {/* Open Graph tags */}
+
+            <meta property="og:title" content="" id="meta-og-title" />
+            <meta property="og:description" content="" id="meta-og-desc" />
+            <meta
+              property="og:url"
+              content="https://www.tu.biz"
+              id="meta-og-url"
+            />
+            <meta property="og:locale" content="en" />
+            <meta property="site_name" content="TU" />
+            <meta
+              name="image"
+              property="og:image"
+              content="https://www.tu.biz//blog-images-english/"
+              id="meta-og-image"
+            />
+
+            {/* Twitter card */}
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:creator" content="@MrEdgarLindo" />
+
+            <meta name="twitter:title" content="  " />
+
+            <meta name="twitter:description" content="" />
+            <meta
+              name="twitter:image"
+              content="https://www.tu.biz/blog-images-english/"
+            />
+          </div>
+        </Head>
       </div>
       <div className="row">
         <div className="col-md-9 table-responsive">
