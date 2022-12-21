5f71ca92ef82169a8d58dbd6aa2c69394b9c87e0
Edgar <elindo586@gmail.com>
2022-12-21 13:04:59 -0500

    carousel

--- components/carousel-main.js
+++ components/carousel-main.js
@@ -51,6 +51,16 @@ const CarouselMain = () => {
             height={315}
           />
         </Carousel.Item>
+
+        <Carousel.Item className="carousel-item ">
+          <Image
+            className="d-block w-100"
+            src="/carousel-main/mobile-valves.png"
+            alt="mobile hydraulic valves"
+            width={1710}
+            height={315}
+          />
+        </Carousel.Item>
       </Carousel>
     </div>
   );
