2da90d3d46eef1de0d0f98a3dc01f7e328e6b626
Edgar <elindo586@gmail.com>
2022-12-21 13:41:58 -0500

    carousel

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Head from "next/head";
-import Carousel from "react-bootstrap/Carousel";
+import CarouselMain from "../components/carousel-main";
 
 const Home = () => {
   return (
@@ -74,53 +74,7 @@ const Home = () => {
       </div>
 
       <div className="row tu-world">
-        <Carousel>
-          <Carousel.Item className="carousel-item active">
-            <Image
-              className="d-block w-100"
-              src="/carousel-main/tu-world.png"
-              alt="tu world"
-              width={1710}
-              height={315}
-            />
-          </Carousel.Item>
-          <Carousel.Item className="carousel-item">
-            <Image
-              className="d-block w-100"
-              src="/carousel-main/carousel-pumps.png"
-              alt="hydraulic pumps"
-              width={1710}
-              height={315}
-            />
-          </Carousel.Item>
-          <Carousel.Item className="carousel-item">
-            <Image
-              className="d-block w-100"
-              src="/carousel-main/carousel-hydraulic-motors.png"
-              alt="hydraulic pumps"
-              width={1710}
-              height={315}
-            />
-          </Carousel.Item>
-          <Carousel.Item className="carousel-item">
-            <Image
-              className="d-block w-100"
-              src="/carousel-main/carousel-valves-manifolds.png"
-              alt="hydraulic pumps"
-              width={1710}
-              height={315}
-            />
-          </Carousel.Item>
-          <Carousel.Item className="carousel-item ">
-            <Image
-              className="d-block w-100"
-              src="/carousel-main/carousel-everything.png"
-              alt="everything industrial"
-              width={1710}
-              height={315}
-            />
-          </Carousel.Item>
-        </Carousel>
+        <CarouselMain />
       </div>
       <div className="row">
         <div className="row" lang="en"></div>
