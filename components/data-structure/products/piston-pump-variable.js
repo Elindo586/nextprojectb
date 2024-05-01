99738ae5a3a25735e1786f98e5ca6ab658686a24
Edgar <elindo586@gmail.com>
2024-05-01 15:49:35 -0400

    a10

--- components/data-structure/products/piston-pump-variable.js
+++ components/data-structure/products/piston-pump-variable.js
@@ -7,48 +7,47 @@ const PistonPumpVariable = () => (
     type="application/ld+json"
     dangerouslySetInnerHTML={{
       __html: `{
-      "@context": "https://schema.org/",
-      "@type": "Product",
-     
-      "image": [
-        "https://www.tu.biz/images/rexrothpump-1.png"
-      ],
-      "name": "Piston Pump",
-      "description": "Variable displacement Piston Pump",
-      "brand": {
-        "@type": "TU",
-        "name": "TU piston pump"
-      },
-      "offers": {
-        "@type": "Offer",
-        "url": "https://www.tu.biz/products/piston-pumps",
-        "itemCondition": "https://schema.org/NewCondition",
-        "availability": "https://schema.org/InStock",
-        
-        "shippingDetails": {
-          "@type": "OfferShippingDetails",
-          
-          "shippingDestination": {
-            "@type": "DefinedRegion",
-            "addressCountry": "US"
+    "@context": "https://schema.org/",
+    "@type": "Product",
+
+    "image": "https://www.tu.biz/images/rexrothpump-1.png",
+    "name": "Piston Pump",
+    "description": "Variable displacement Piston Pump",
+    "brand": {
+      "@type": "TU",
+      "name": "TU piston pump"
+    },
+    "offers": {
+      "@type": "Offer",
+      "url": "https://www.tu.biz/products/piston-pumps",
+      "itemCondition": "https://schema.org/NewCondition",
+      "availability": "https://schema.org/InStock",
+
+      "shippingDetails": {
+        "@type": "OfferShippingDetails",
+
+        "shippingDestination": {
+          "@type": "DefinedRegion",
+          "addressCountry": "US"
+        },
+        "deliveryTime": {
+          "@type": "ShippingDeliveryTime",
+          "handlingTime": {
+            "@type": "QuantitativeValue",
+            "minValue": 0,
+            "maxValue": 1,
+            "unitCode": "DAY"
           },
-          "deliveryTime": {
-            "@type": "ShippingDeliveryTime",
-            "handlingTime": {
-              "@type": "QuantitativeValue",
-              "minValue": 0,
-              "maxValue": 1,
-              "unitCode": "DAY"
-            },
-            "transitTime": {
-              "@type": "QuantitativeValue",
-              "minValue": 1,
-              "maxValue": 3,
-              "unitCode": "DAY"
-            }
+          "transitTime": {
+            "@type": "QuantitativeValue",
+            "minValue": 1,
+            "maxValue": 3,
+            "unitCode": "DAY"
           }
         }
       }
+    }
+  }
       `,
     }}
   />
