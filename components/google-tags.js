fd6aca1acb9df931c9186465d4ecc39c138cd395
Edgar <elindo586@gmail.com>
2024-06-24 13:31:13 -0400

    tags

--- components/google-tags.js
+++ components/google-tags.js
@@ -81,7 +81,7 @@ const GoogleTags = () => {
             }`,
           }}
         />
-        <Script
+        {/* <Script
           id="google-tag-manager-1"
           strategy="afterInteractive"
           dangeroulySetInnerHTML={{
@@ -91,7 +91,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KXZRG4LK'); `,
           }}
-        />
+        /> */}
 
         <noscript>
           <iframe
