4ad402c0057b1c067c9d0c5f77d01f1727a9ca2f
Edgar <elindo586@gmail.com>
2024-06-27 10:37:35 -0400

    footer quick contact

--- components/contact-quick-spanish.js
+++ components/contact-quick-spanish.js
@@ -13,16 +13,41 @@ const ContactQuickSpanish = () => {
       </div>
       <div className="row">
         <div className="col quick-contact-english-text">
-          <p>
-            {" "}
-            Tel: <a href="tel:+15862213095">+1-586-221-3095</a>{" "}
-          </p>
+          <div className="row">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="200"
+              height="20"
+              fill="currentColor"
+              className="bi bi-telephone-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
+              />
+            </svg>
+          </div>
+          <div className="row">
+            <a href="tel:+15862213095">+1-586-221-3095</a>
+          </div>
         </div>
         <div className="col quick-contact-english-text">
-          <p>
-            {" "}
-            Email: <a href="mailto:info@tu.biz">info@tu.biz</a>{" "}
-          </p>
+          <div className="row">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="200"
+              height="20"
+              fill="currentColor"
+              className="bi bi-envelope-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
+            </svg>
+          </div>
+          <div className="row">
+            <a href="mailto:info@tu.biz">info@tu.biz</a>
+          </div>
         </div>
       </div>
     </div>
--- components/footer.js
+++ components/footer.js
@@ -9,103 +9,36 @@ const Footer = () => {
     <div>
       <footer className="main-footer">
         <div className="container">
-          <div className="row ">
+          <div className="row">
             <div className=" col footer-privacy">PRIVACY POLICY</div>
           </div>
           <div className="row">
+            <div className="col">
+              <div className="row">
+                <div className="col-sm-6">
+                  {" "}
+                  <p className="copyright">Copyright © {fullYear()} TU </p>
+                </div>
+                <div className="col-sm-6">
+                  <p className="copyright">All Rights Reserved </p>
+                </div>
+              </div>
+            </div>
             <div className="col ">
-              <p className="copyright">
-                Copyright © {fullYear()} TU - All Rights Reserved{" "}
-              </p>
-            </div>
-            <div className="col dev-by">
-              <span className="dev-by2">Developed by:</span>{" "}
-              <a
-                className="edgar"
-                href="https://www.edgarlindo.com/"
-                target="blank"
-              >
-                Edgar Lindo
-              </a>
-            </div>
-
-            <div className="dev-by">
-              {/* <Row>
-                <Col className="social-icon-col">
+              <div className="row">
+                <div className="col-sm-6">
+                  <p className="dev-by">Developed by:</p>
+                </div>
+                <div className="col-sm-6">
                   <a
-                    className="footer-icon"
-                    href="https://www.linkedin.com/in/edgarlindo"
+                    className="edgar"
+                    href="https://www.edgarlindo.com/"
                     target="blank"
                   >
-                    <Image
-                      src="/images/footer-images/footer-linkedin.png"
-                      id="youtube-spanish"
-                      alt="youtube spanish logo"
-                      width={25}
-                      height={25}
-                      className="footer-icon"
-                      title="LinkedIn"
-                    />
+                    Edgar Lindo
                   </a>
-
-                  <a
-                    className="footer-icon"
-                    href="https://twitter.com/MrEdgarLindo"
-                    target="blank"
-                  >
-                    <Image
-                      src="/images/footer-images/footer-twitter.png"
-                      id="youtube-spanish"
-                      alt="youtube spanish logo"
-                      width={25}
-                      height={25}
-                      title="Twitter"
-                    />
-                  </a>
-                  <a
-                    className="footer-icon"
-                    href="https://www.facebook.com/RealEdgarLindo"
-                    target="blank"
-                  >
-                    <Image
-                      src="/images/footer-images/footer-facebook.png"
-                      id="youtube-spanish"
-                      alt="youtube spanish logo"
-                      width={25}
-                      height={25}
-                      title="Facebook"
-                    />
-                  </a>
-                  <a
-                    className="footer-icon"
-                    href="https://www.youtube.com/c/EdgarLindoEL/videos"
-                    target="blank"
-                  >
-                    <Image
-                      src="/images/footer-images/footer-youtube.png"
-                      id="youtube-spanish"
-                      alt="youtube spanish logo"
-                      width={25}
-                      height={25}
-                      title="YouTube English"
-                    />
-                  </a>
-                  <a
-                    className="footer-icon"
-                    href="https://www.youtube.com/c/EdgarLindo/videos"
-                    target="blank"
-                  >
-                    <Image
-                      src="/images/footer-images/footer-youtube.png"
-                      id="youtube-spanish"
-                      alt="youtube spanish logo"
-                      width={25}
-                      height={25}
-                      title="YouTube Spanish"
-                    />
-                  </a>
-                </Col>
-              </Row> */}
+                </div>
+              </div>
             </div>
           </div>
         </div>
